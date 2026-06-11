31ce8b3ce33bd5246b86acf83208a74f61b91714
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:11:33 +0000

    multiple if statement

---
+++ chapter_06/03_multiple_if_statements.py
@@ -0,0 +1,22 @@
+
+a = int(input("enter your age : "))
+
+# if statement no : 1
+
+if (a%2 == 0):
+    print("a is even")
+    
+
+# if statement no : 2
+
+if(a>18):
+    print("you are above the age ogf consent")
+    print("good for you")
+
+elif(a<0):
+    print("you are entering invalid age")
+
+else :
+    print("you are entering invalid age")
+
+print("end of program")
