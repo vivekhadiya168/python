41b75ac50daa4a19673d8fbd889f203d1144d40c
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 05:59:07 +0000

    changes in datatype file

--- chapter_02/02_datatypes.py
+++ chapter_02/02_datatypes.py
@@ -4,6 +4,6 @@ b = 5.22 # b is a floating point number
 
 c = "harry"  # c is a string
 
-d = false # d is a boolean
+d = false # type: ignore # d is a boolean
 
-e = none    # e is a none type variable
+e = none    # type: ignore # e is a none type variable
