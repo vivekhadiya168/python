ed3e6eddbca1ce7b474001859ccf3646b03421a2
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 14:03:31 +0000

    sets in python

---
+++ chapter_05/03_sets.py
@@ -0,0 +1,7 @@
+
+e = set()                  # to create empty set  if we use s = {}, it create empty dictionary
+# print(type(e))
+
+
+s = {1,5,32,5,1,"vivek"}        # set ma repetation allow nathi. output ma 1 value 1 var j aavse
+print(s)
