53c9b4b8a1e5887e5cf11f79c53850c253cafbbb
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 07:42:27 +0000

    strings in python

---
+++ chapter_03/01_strings.py
@@ -0,0 +1,13 @@
+# ways to write a string and slicing
+
+# name1 = "vivek"
+# name2 = 'vivek'
+# name3 = '''vivek'''
+
+name = "vivek"
+
+slice1 = name[2]
+print(slice1)
+
+slice2 = name[1:4]   # [] ni andar je range aapsu e follow lkarse. last element nai aape means [1:4] lakshu to 4 nai aape
+print(slice2)
