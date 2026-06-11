69464891a1e5acc2742a1016719f6d8d8627e130
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 07:43:31 +0000

    escape sequence character in python

---
+++ chapter_03/04_escape_sequence.py
@@ -0,0 +1,7 @@
+a = "vivek is a good boy \nbut not a \"bad\" boy\tvivek\\"
+print(a)
+
+# \n  biji line ma lakhva
+# \t  tab jetli space mukva
+# \" doubble quote ni andar doubble quote karva
+# \\ backslash karvu hoy to \\ lakhvu. ek var \ aavi jase
