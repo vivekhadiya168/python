500f45b1cb2fd58bbae52505ffb38cf77c27a344
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:15:06 +0000

    pass in python

---
+++ chapter_07/08_pass.py
@@ -0,0 +1,11 @@
+
+
+for i in range(100):
+    pass               # statement skip thay jay means aapde for loop ne em j rakhi ne aagal vadhi shakiye
+
+
+i = 1
+
+while(i<6):
+    print(i)
+    i+=1 
