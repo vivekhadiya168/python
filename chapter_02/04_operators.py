489b60598f69ee79e6635f16558811da9d128851
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 05:55:52 +0000

    operators in python

---
+++ chapter_02/04_operators.py
@@ -0,0 +1,34 @@
+# Arithmetic operators 
+
+a = 34
+b = 4
+c = a + b
+print(c)
+
+# Assignment operators
+# means ek variable ni andar value assign kariye chiye
+
+a = 4 - 2
+print(a)
+
+b = 6
+b += 3 # b ni value ma 3 add thase 
+print(b)
+
+c = 5
+c -= 3     # c ni value mathi 3 - thase 
+print(c)
+
+# comparision operators 
+
+d = 7 < 4
+print(d)
+
+# Logical operators
+
+# or
+# and
+# not    # true ne false kare nr false ne true kare
+
+
+
