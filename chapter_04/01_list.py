fbf7f259acae110b78e08994f8ee00ec862e8452
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 14:01:38 +0000

    list in python

---
+++ chapter_04/01_list.py
@@ -0,0 +1,7 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print (friends[0])
+
+friends[0] = "Graps"      # lists are mutable. list ne change kari shakiye
+print(friends[0])
+print(friends[1:4])
