33e2adce3dd2189bfdeb696cab5003bb4facf47f
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:10:58 +0000

    conditional expression in python

---
+++ chapter_06/01_conditional_expression.py
@@ -0,0 +1,12 @@
+# if else statement
+
+a = int(input("enter your age : "))
+
+if (a>=18):
+    print("you are above the age ogf consent")
+    print("good for you")
+
+else :
+    print("you are below the age of consent")
+
+print("end of program")
