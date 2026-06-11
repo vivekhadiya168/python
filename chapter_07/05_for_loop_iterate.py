6ceb2ac46ee9596cf29e86a7e3fd980382837b13
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:14:29 +0000

    for loop iterate

---
+++ chapter_07/05_for_loop_iterate.py
@@ -0,0 +1,18 @@
+
+# for loops with lists
+
+l = [1, 2, 3, 4, 5, 6, 7]
+for i in l:
+    print(i)
+
+# for loops with tuples
+
+t = (4,7,56,68)
+for i in t :
+    print(i)
+
+# for loops with string
+
+s = "vivekk"
+for i in s :
+    print(i)
