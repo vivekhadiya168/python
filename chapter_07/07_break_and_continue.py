9ff5dc9521ba9f475563593cd5bf5309be45755f
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:14:56 +0000

    break and continue in python

---
+++ chapter_07/07_break_and_continue.py
@@ -0,0 +1,10 @@
+
+for i in range(100):
+    if(i == 8):
+        break        # exit the loop right now
+    print(i)
+
+for i in range(100):
+    if(i == 8):
+        continue     # skip this iteration
+    print(i)
