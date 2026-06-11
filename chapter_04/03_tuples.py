19f3498021947e4718388ba860409701bf430dfc
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 14:02:04 +0000

    tuples in python

---
+++ chapter_04/03_tuples.py
@@ -0,0 +1,10 @@
+x = (1,2,5,6)           # tuple
+# print(type(x))
+
+z = ()                    # empty tuple
+# print(type(z))
+
+a = (1,3,45)
+# a[0] = 456                      # tuplr is immutable. change na thay
+
+print(a)
