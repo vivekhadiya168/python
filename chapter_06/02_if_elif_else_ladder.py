95640412935546d5fe317a5e2f18f4d9446341c2
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 19:11:18 +0000

    if elif else ladder in python

---
+++ chapter_06/02_if_elif_else_ladder.py
@@ -0,0 +1,15 @@
+# if elif else ladder
+
+a = int(input("enter your age : "))
+
+if (a>=18):
+    print("you are above the age ogf consent")
+    print("good for you")
+
+elif(a<0):
+    print("you are entering invalid age")
+
+else :
+    print("you are entering invalid age")
+
+print("end of program")
