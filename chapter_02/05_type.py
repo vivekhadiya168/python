c7e0ef15268b55caf83dd1e8c27c42a6d5fc3d83
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-11 05:56:14 +0000

    type function in python

---
+++ chapter_02/05_type.py
@@ -0,0 +1,20 @@
+a = 32
+m = type(a)
+print(m)
+
+b = 34.65
+n = type(b)
+print(n)
+
+c = "vivek"
+o = type(c)
+print(o)
+
+d = True
+p = type(d)
+print(p)
+
+
+aa = 33.45
+b = int(aa)  # means b = a but type of b is float
+print(type(aa))
