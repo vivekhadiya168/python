e668a46448c7782c2eeb49fa1afca17af4aa838c
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-17 07:05:57 +0000

    function with arguments

---
+++ chapter_08/03_functions_with_arguments.py
@@ -0,0 +1,10 @@
+
+def goodDay(name, ending):
+    print("Good day, " + name)
+    print(ending)
+    return "ok"                             # value return kare  aa value a ni andar assign thase
+
+a = goodDay("vivek", "thank you")              # function with argument
+print(a)                      # a ni return value aape che
+
+# return,  jyare koi ni value aapde joti hoy tyare call karave
