ce906472694026c7b9dbc9877a7b0fd50ca5f81f
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-17 18:12:22 +0000

    instance and class attribute

---
+++ chapter_10/02_instance_vs_class_attribute.py
@@ -0,0 +1,11 @@
+
+class Employee:
+    language= "py"             # this is class attribute
+    salary = 120000
+
+vivek = Employee()
+vivek.language = "javascript"    # instance attribute
+print(vivek.language)
+
+
+# instance attribute hoy e preference ley means aapde same attribute ne print karavine data change kariye to instance attribute ma je data hoy e print thase.
