e99d2b1127a82591eda6f1257a901ec80b1c7b9c
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-17 07:06:13 +0000

    default argument in function

---
+++ chapter_08/04_default_argument.py
@@ -0,0 +1,7 @@
+
+def goodDay(name, ending = "thank you"):  # aaya ending ninvalue default j aapi didhi che means call karavti vakhte value nai aapiye to defalut value j call thay jase
+    print(f"Good day, {name}")
+    print(ending)
+
+goodDay("vivek", "thanks")  # aaya thanks malse
+goodDay("rohan")            # aaya thank you malse
