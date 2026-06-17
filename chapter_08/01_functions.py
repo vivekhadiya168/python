a03422377247398796aaf1b5a125c2ce0296e130
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-17 07:05:23 +0000

    functions in python

---
+++ chapter_08/01_functions.py
@@ -0,0 +1,22 @@
+
+# a = int(input("enter your number : "))
+# b = int(input("enter your number : "))
+# c = int(input("enter your number : "))
+
+
+# avg = ( a + b + c)/3
+
+# print(avg)
+
+def avg():                                           # function defination
+    a = int(input("enter your number : "))
+    b = int(input("enter your number : "))
+    c = int(input("enter your number : "))
+
+
+    avg = ( a + b + c)/3
+
+    print(avg)
+
+avg()                                                    # function call
+avg()             # jetli var print karavvu hoy etli var lakhvu
