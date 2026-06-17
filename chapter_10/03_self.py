eea3e818c52dcfd28c5ad760feab3f4d9d1eabe3
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-17 18:12:43 +0000

    self function in python

---
+++ chapter_10/03_self.py
@@ -0,0 +1,16 @@
+
+class Employee:
+    language= "py"
+    salary = 120000
+
+    def getInfo(self):                 # vivek name no object aapde pass karavyo je accept nata karta etle self lakhyu
+        print(f"the language is {self.language}. the salary is {self.salary}")
+
+    @staticmethod                       # jo aapde aa greet mate object pass na karavvo hoy to static method use thay. means greet ne object ni jarur nzthi
+    def greet():
+        print("good morning")
+
+vivek = Employee()
+vivek.language = "javascript"
+vivek.getInfo()
+vivek.greet()
